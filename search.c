2c11304e2ef7d6dd71a706be5c250b0a3d1d9785
youckyhaohaoxiedaima <1073958100@qq.com>
2020-07-04 23:34:49 +0800

    bfs not finish

--- search.c
+++ search.c
@@ -1,11 +1,12 @@
-#include "search.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include"search.h"
+#include<stdio.h>
+#include<stdlib.h>
 #include "graph.h"
+#include "search.h"
 #include <stdlib.h>
 #include "stats.h"
 #include "priority.h"
-#include "tools.h"
+#include <stdio.h>
 
 static int compare(const void *a, const void *b);
 static void assign(void *a, void *b);
@@ -14,22 +15,23 @@ typedef struct {
     unsigned long long dis;
 } vertexInfo;
 
-static void dfs(Graph* this, int start, int end, int *pth, int *vis); 
-int _graphBFS(struct graph *, int, int, int  *);
-int *_graphDijkstra(struct graph *, int, int);
+/* Variables needed for recursion */
+int t = 0;
+int ID;
+int flag = 0;
+int visit[mainGraph->_vertexMax];          
+int path[mainGraph->_edgeNum];
+void dfs(Graph* this, int start, int end); 
 
-int _graphBFS(struct graph *this, int start, int end, int *res) {
-    return 0;
-}
-
+/*Main function*/
+int* graphDFS(char *filename, int start, int end);
+int* graphBFS(Graph *this, int start, int end);
+int* graphDijkstar(char *filename, int start, int end);
+int* shortestPath(int u, int v, char algorithm[]);
 
 /*Recursive auxiliary function*/
-static void dfs(Graph* this, int start, int end, int *path, int *visit)
+static void dfs(Graph* this, int start, int end)
 {
-    static int flag = 0;
-    static int t = 0;
-    static int ID;
-
 	if (start == end) {
 		for (int i = 0; i < t; i++) {
 			printf("%d ", path[i]);           
@@ -37,7 +39,6 @@ static void dfs(Graph* this, int start, int end, int *path, int *visit)
 		flag = 1;                             //mark end
 		return;
 	}
-
 	ID = this->_vertexList[start];
 	if (ID == -1)return;                      //the start point is sink
 	for (; this->_edgeList[ID].to != -1; ID = this->_edgeList[ID].nextID) {
@@ -45,7 +46,7 @@ static void dfs(Graph* this, int start, int end, int *path, int *visit)
 	    if (visit[num] != 1) {
             visit[num] = 1;
             path[t++] = num;
-            dfs(this, num, end, path, visit);
+            dfs(this, num, end);
             if (flag == 0) {                 //Avoid unnecessary loops
                 visit[num] = 0;
                 path[--t] = 0;
@@ -54,25 +55,75 @@ static void dfs(Graph* this, int start, int end, int *path, int *visit)
 	}
 }
 
-//这几个算法也是graph类里面的吗
-int *_graphDFS(Graph *this, int start, int end)
+int* _graphDFS(Graph *this, int start, int end)
 {
-    /*char *visit = (char *) calloc(sizeof(char), this->_vertexMax);
-    int *path = (int *) calloc(this->_vertexNum, sizeof(int));
-
-    int ID = this->_vertexList[start];
-	if (ID == -1) { 
-        return NULL;
-    }
+    mainGraph = this;
+    ID = this->_vertexList[start];
+	if (ID == -1)return;
 	visit[start] = 1;
 	path[t++] = start;
 	dfs(this, start, end);
-	return path;*/
-    return NULL;
+	return path;
 }
+    
+int* vis;
+ int* BFS(char *filename, int start, int end)
+ {
+     if (!mainGraph) {
+        initGraph(filename);
+    }
+
+    int *path = NULL;
+    mainGraph->vtable.graphBFS(mainGraph, start, end, path);
+    return path;
+ }
+
+int _graphBFS(struct graph *this, int start, int end, int *res) {
+{
+	unsigned long long int* vertexDis = NULL, vis = NULL;
+	vertexDis = (unsigned long long int*) malloc(sizeof(unsigned long long int) * this->_vertexMax);
+	vis = (unsigned long long int*) malloc(sizeof(unsigned long long int) * this->_vertexMax);
+	vertexInfo tmp = {
+		.pos = start,
+		.dis = vertexDis[start]
+	};
+    for (int i = 0; i < this->_vertexMax; ++i) {
+		vis[i] = 0;
+		vertexDis[i] = 1e9;
+	}
+
+	priorityQueue* q = initPriorityQueue(this->_vertexMax, sizeof(vertexInfo), compare, assign);
+
+	vertexDis[start] = 0;
+	vertexInfo* curr = NULL;
+
+	q->vtable.push(q, &start);
+	vertexDis[start] = 0;
+	vis[start] = 1;
+	while (!q->vtable.isEmpty(q))
+	{
+		int now = q->vtable.pop(q);		
+		vis[now] = 0;
+		for (int id= this->_vertexList[now]; this->_edgeList[id].to != -1; id = this->_edgeList[id].nextID)
+		{
+			int next = this->_edgeList[id].to;
+			if (vertexDis[next] > vertexDis[now] + this->_edgeList[id].weight)
+			{
+				vertexDis[next] = vertexDis[now] + this->_edgeList[id].weight;
+				if (vis[next])continue; //已经在队列里的就跳过
+				vis[next] = 1;
+				q->vtable.push(next, &start);
+			}
+		}
+	}
+	q->vtable.fini(q);
+
+	return ;
+}
+ 
 
 
-int *graphDFS(char *filename, int start, int end)
+ int* graphDFS(char *filename, int start, int end)
 {
    if (!mainGraph) {
         initGraph(filename);
@@ -80,40 +131,37 @@ int *graphDFS(char *filename, int start, int end)
     return mainGraph->vtable.graphDFS(mainGraph, start, end);
 }
 
-//不知道这个函数如何传入文件，所以就先用maingraph了
-int* shortestPath(int u, int v, char algorithm[])
+int* shortestPath(int u, int v, char algorithm[], char filename)
 {
+    if (!mainGraph) {
+        initGraph(filename);
+    }
     int *shortPath;
-    /*if (strcmp(algorithm, "DFS") == 0) {
+    if (strcmp(algorithm, "DFS") == 0) {
         shortPath = DFS(mainGraph, u, v);
     } else if (strcmp(algorithm, "BFS" ) == 0) {
         shortPath = BFS(mainGraph, u, v);
     } else if (strcmp(algorithm, "BFS" ) == 0) {
         shortPath = Dijkstar(mainGraph, u, v);
-    } */
+    } 
     return shortPath;
-}
-
+}   
 
 int* graphDijkstar(char *filename, int start, int end) {
     if (!mainGraph) {
         initGraph(filename);
     }
 
-    return mainGraph->vtable.graphDijkstra(mainGraph, start, end);
+    int *path = NULL;
+    mainGraph->vtable.graphDijkstra(mainGraph, start, end, path);
+    return path;
 }
 
-int *_graphDijkstra(struct graph *this, int start, int end) {
+int _graphDijkstra(struct graph *this, int start, int end, int *path) {
     unsigned long long int *vertexDis = NULL;
     vertexDis = (unsigned long long int *) malloc(sizeof(unsigned long long int) * this->_vertexMax);
     for (int i = 0; i < this->_vertexMax; ++i) {
         vertexDis[i] = -1;
-    }
-    int *pre = (int *) malloc(sizeof(int) * this->_vertexMax);
-    char *vis = (char *) calloc(sizeof(char), this->_vertexMax);
-    
-    for (int i = 0; i < this->_vertexMax; ++i) {
-        pre[i] = -1;
     }
 
     priorityQueue *q = initPriorityQueue(this->_vertexMax, sizeof(vertexInfo), compare, assign);
@@ -129,16 +177,7 @@ int *_graphDijkstra(struct graph *this, int start, int end) {
 
     q->vtable.push(q, &tmp);
     while(q->vtable.size(q)){
-        do {
-            curr = q->vtable.pop(q);
-        } while (q->vtable.size(q) > 0 && vis[curr->pos] == 1);
-        
-        if (vis[curr->pos] == 1) {
-            break;
-        }
-        
-        vis[curr->pos] = 1;
-
+        curr = q->vtable.pop(q);;
         int from = curr->pos;
         for(int ID = this->_vertexList[from]; ID != -1 ; ID = this->_edgeList[ID].nextID) {
             if(vertexDis[this->_edgeList[ID].to] > this->_edgeList[ID].weight + curr->dis) {
@@ -146,7 +185,6 @@ int *_graphDijkstra(struct graph *this, int start, int end) {
                 tmp.pos = this->_edgeList[ID].to;
                 tmp.dis = vertexDis[this->_edgeList[ID].to];
                 q->vtable.push(q, &tmp);
-                pre[this->_edgeList[ID].to] = from;
             }
         }
     }
@@ -154,17 +192,12 @@ int *_graphDijkstra(struct graph *this, int start, int end) {
 
     int cur = end;
     int top = 1;
-    int *path = (int *) calloc(this->_vertexNum, sizeof(int));
+    path = (int *) calloc(this->_vertexNum, sizeof(int));
     path[0] = end;
 
-    for ( ; cur != start && pre[cur] != -1; ++top) {
-        cur = pre[cur];
-        path[top] = cur;
-    }
     path[top] = -1;
-
     free(vertexDis);
-    return path;
+    return top - 1;
 }
 
 static int compare(const void *a, const void *b) {
